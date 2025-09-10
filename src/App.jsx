e326f7492cf90cca934bd0856c8d5b4648a4fac5
Ibinola <ibinolamichael1@gmail.com>
2025-09-10 14:22:05 +0100

    remove unused route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Expenses from "./pages/Expenses";
 import Summary from "./pages/Summary";
-import ReceiptScanner from "./components/extractReceiptWithGoogleVision";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/expenses" element={<Expenses />} />
       <Route path="/summary" element={<Summary />} />
-      <Route path="/google" element={<ReceiptScanner />} />
     </Routes>
   );
 }
